6f6805350c9272e58b2360d3ffa891d6a5859760
Youssouf Tangara <ytangara2003@gmail.com>
2025-10-19 14:28:12 +0100

    setup of the projection and springdatarest

--- customer-service/src/main/java/net/youssouf/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/net/youssouf/customerservice/CustomerServiceApplication.java
@@ -1,7 +1,11 @@
 package net.youssouf.customerservice;
 
+import net.youssouf.customerservice.entities.Customer;
+import net.youssouf.customerservice.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
@@ -10,4 +14,27 @@ public class CustomerServiceApplication {
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(CustomerRepository customerRepository) {
+		return args -> {
+			customerRepository.save(Customer.builder()
+					.name("Youssouf")
+					.email("youssouf@gmail.com")
+					.build());
+			customerRepository.save(Customer.builder()
+					.name("Tangara")
+					.email("tangara@gmail.com")
+					.build());
+			customerRepository.save(Customer.builder()
+					.name("salma")
+					.email("salma@gmail.com")
+					.build());
+
+			customerRepository.findAll().forEach(customer ->{
+				System.out.println("========================================================");
+				System.out.println(customer.toString());
+				System.out.println("========================================================");
+			} );
+		};
+	}
 }
